4bbc4d929fa4309735bee9378fb40aeae4d39e8f
Daniel Schuette <d.schuette@online.de>
2018-09-06 21:57:24 -0400

    add to numpy_random.py

--- code/statistics/numpy_random.py
+++ code/statistics/numpy_random.py
@@ -2,7 +2,9 @@
 
 import sys
 
+import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
 # initialize a random seed
 try:
@@ -21,3 +23,21 @@ np.random.seed(seed)
 sample = np.array([1, 2, 3, 4, 5, 6])
 rand_sampled = np.random.choice(sample)
 print(rand_sampled)
+
+# the numpy.random module provides a suit of functions to draw random
+# samples from probability distributions, e.g. the Poisson distribution
+# parameters: lambda (`lamb'), sample size (`size')
+lamb, size = 5, 1000
+poisson_dist = np.random.poisson(lamb, size)
+
+# seaborn's `distplot' can be used to visualize probability distributions
+sns.distplot(poisson_dist)
+# plt.show("hold")
+
+# to randomly shuffle values in a list or numpy array, `np.random.shuffle'
+# can be used
+sorted_list = list(range(10))
+print("sorted: {}".format(sorted_list))
+shuffled_list = sorted_list.copy()  # `np.random.shuffle' will shuffle in-place
+np.random.shuffle(shuffled_list)
+print("shuffled: {}".format(shuffled_list))
