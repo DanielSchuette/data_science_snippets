54267d18865a50f4a391ed0c53499f9962fff8ff
Daniel Schuette <d.schuette@online.de>
2018-09-09 23:00:41 -0400

    add simulations.py

---
+++ code/statistical_simulation/simulations.py
@@ -0,0 +1,23 @@
+#!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+
+
+def main():
+    """
+    examples of how to use python for statistical simulation
+
+    simulation is a way to model random events by repeatedly
+    sampling from a probability distribution that is likely
+    to underlie the actual real-life event
+
+    usually, the workflow is as follows:
+        1. define possible outcomes
+        2. assign probabilities to outcomes
+        3. define the relationship between outcomes/variables
+        4. sample repeatedly from the appropriate probability
+           distribution to simulate possible outcomes
+        5. visualize/analyze outcomes
+    """
+
+
+if __name__ == "__main__":
+    main()
